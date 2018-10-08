e63a5ab4dd846a491ac7fb1be0f7e743d6b0ab2a
nisen <imnisen@gmail.com>
2018-10-08 18:04:19 +0800

    tmp save, not finished

---
+++ tests/txscript/test_script_num.py
@@ -0,0 +1,231 @@
+import unittest
+from txscript.script_num import *
+
+
+def hex_to_bytes(s):
+    return bytes.fromhex(s)
+
+
+class TestScriptNum(unittest.TestCase):
+    def test_bytes(self):
+        tests = [
+            {"num": 0, "serialized": bytes()},
+            {"num": 1, "serialized": hex_to_bytes("01")},
+            {"num": -1, "serialized": hex_to_bytes("81")},
+            {"num": 127, "serialized": hex_to_bytes("7f")},
+            {"num": -127, "serialized": hex_to_bytes("ff")},
+            {"num": 128, "serialized": hex_to_bytes("8000")},
+            {"num": -128, "serialized": hex_to_bytes("8080")},
+            {"num": 129, "serialized": hex_to_bytes("8100")},
+            {"num": -129, "serialized": hex_to_bytes("8180")},
+            {"num": 256, "serialized": hex_to_bytes("0001")},
+            {"num": -256, "serialized": hex_to_bytes("0081")},
+            {"num": 32767, "serialized": hex_to_bytes("ff7f")},
+            {"num": -32767, "serialized": hex_to_bytes("ffff")},
+            {"num": 32768, "serialized": hex_to_bytes("008000")},
+            {"num": -32768, "serialized": hex_to_bytes("008080")},
+            {"num": 65535, "serialized": hex_to_bytes("ffff00")},
+            {"num": -65535, "serialized": hex_to_bytes("ffff80")},
+            {"num": 524288, "serialized": hex_to_bytes("000008")},
+            {"num": -524288, "serialized": hex_to_bytes("000088")},
+            {"num": 7340032, "serialized": hex_to_bytes("000070")},
+            {"num": -7340032, "serialized": hex_to_bytes("0000f0")},
+            {"num": 8388608, "serialized": hex_to_bytes("00008000")},
+            {"num": -8388608, "serialized": hex_to_bytes("00008080")},
+            {"num": 2147483647, "serialized": hex_to_bytes("ffffff7f")},
+            {"num": -2147483647, "serialized": hex_to_bytes("ffffffff")},
+
+            # Values that are out of range for data that is interpreted as
+            # numbers, but are allowed as the result of numeric operations.
+            {"num": 2147483648, "serialized": hex_to_bytes("0000008000")},
+            {"num": -2147483648, "serialized": hex_to_bytes("0000008080")},
+            {"num": 2415919104, "serialized": hex_to_bytes("0000009000")},
+            {"num": -2415919104, "serialized": hex_to_bytes("0000009080")},
+            {"num": 4294967295, "serialized": hex_to_bytes("ffffffff00")},
+            {"num": -4294967295, "serialized": hex_to_bytes("ffffffff80")},
+            {"num": 4294967296, "serialized": hex_to_bytes("0000000001")},
+            {"num": -4294967296, "serialized": hex_to_bytes("0000000081")},
+            {"num": 281474976710655, "serialized": hex_to_bytes("ffffffffffff00")},
+            {"num": -281474976710655, "serialized": hex_to_bytes("ffffffffffff80")},
+            {"num": 72057594037927935, "serialized": hex_to_bytes("ffffffffffffff00")},
+            {"num": -72057594037927935, "serialized": hex_to_bytes("ffffffffffffff80")},
+            {"num": 9223372036854775807, "serialized": hex_to_bytes("ffffffffffffff7f")},
+            {"num": -9223372036854775807, "serialized": hex_to_bytes("ffffffffffffffff")},
+        ]
+
+        for c in tests:
+            self.assertEqual(ScriptNum(c['num']).bytes(), c['serialized'])
+
+    def test_make_script_num(self):
+        tests = [
+            # # Minimal encoding must reject negative 0.
+            # {"serialized": hex_to_bytes("80"), "num": 0, "numLen": defaultScriptNumLen, "minimalEncoding": True,
+            #  "err": ErrorCode.ErrMinimalData},
+            #
+            # # Minimally encoded valid values with minimal encoding flag.
+            # # Should not error and return expected integral number.
+            # {"serialized": bytes(), "num": 0, "numLen": defaultScriptNumLen, "minimalEncoding": True, "err": None},
+            # {"serialized": hex_to_bytes("01"), "num": 1, "numLen": defaultScriptNumLen, "minimalEncoding": True,
+            #  "err": None},
+            # {"serialized": hex_to_bytes("81"), "num": -1, "numLen": defaultScriptNumLen, "minimalEncoding": True,
+            #  "err": None},
+            # {"serialized": hex_to_bytes("7f"), "num": 127, "numLen": defaultScriptNumLen, "minimalEncoding": True,
+            #  "err": None},
+            # {"serialized": hex_to_bytes("ff"), "num": -127, "numLen": defaultScriptNumLen, "minimalEncoding": True,
+            #  "err": None},
+            # {"serialized": hex_to_bytes("8000"), "num": 128, "numLen": defaultScriptNumLen, "minimalEncoding": True,
+            #  "err": None},
+            # {"serialized": hex_to_bytes("8080"), "num": -128, "numLen": defaultScriptNumLen, "minimalEncoding": True,
+            #  "err": None},
+            # {"serialized": hex_to_bytes("8100"), "num": 129, "numLen": defaultScriptNumLen, "minimalEncoding": True,
+            #  "err": None},
+            # {"serialized": hex_to_bytes("8180"), "num": -129, "numLen": defaultScriptNumLen, "minimalEncoding": True,
+            #  "err": None},
+            # {"serialized": hex_to_bytes("0001"), "num": 256, "numLen": defaultScriptNumLen, "minimalEncoding": True,
+            #  "err": None},
+            # {"serialized": hex_to_bytes("0081"), "num": -256, "numLen": defaultScriptNumLen, "minimalEncoding": True,
+            #  "err": None},
+            # {"serialized": hex_to_bytes("ff7f"), "num": 32767, "numLen": defaultScriptNumLen, "minimalEncoding": True,
+            #  "err": None},
+            # {"serialized": hex_to_bytes("ffff"), "num": -32767, "numLen": defaultScriptNumLen, "minimalEncoding": True,
+            #  "err": None},
+            # {"serialized": hex_to_bytes("008000"), "num": 32768, "numLen": defaultScriptNumLen, "minimalEncoding": True,
+            #  "err": None},
+            # {"serialized": hex_to_bytes("008080"), "num": -32768, "numLen": defaultScriptNumLen,
+            #  "minimalEncoding": True, "err": None},
+            # {"serialized": hex_to_bytes("ffff00"), "num": 65535, "numLen": defaultScriptNumLen, "minimalEncoding": True,
+            #  "err": None},
+            # {"serialized": hex_to_bytes("ffff80"), "num": -65535, "numLen": defaultScriptNumLen,
+            #  "minimalEncoding": True, "err": None},
+            # {"serialized": hex_to_bytes("000008"), "num": 524288, "numLen": defaultScriptNumLen,
+            #  "minimalEncoding": True, "err": None},
+            # {"serialized": hex_to_bytes("000088"), "num": -524288, "numLen": defaultScriptNumLen,
+            #  "minimalEncoding": True, "err": None},
+            # {"serialized": hex_to_bytes("000070"), "num": 7340032, "numLen": defaultScriptNumLen,
+            #  "minimalEncoding": True, "err": None},
+            # {"serialized": hex_to_bytes("0000f0"), "num": -7340032, "numLen": defaultScriptNumLen,
+            #  "minimalEncoding": True, "err": None},
+            # {"serialized": hex_to_bytes("00008000"), "num": 8388608, "numLen": defaultScriptNumLen,
+            #  "minimalEncoding": True, "err": None},
+            # {"serialized": hex_to_bytes("00008080"), "num": -8388608, "numLen": defaultScriptNumLen,
+            #  "minimalEncoding": True, "err": None},
+            # {"serialized": hex_to_bytes("ffffff7f"), "num": 2147483647, "numLen": defaultScriptNumLen,
+            #  "minimalEncoding": True, "err": None},
+            # {"serialized": hex_to_bytes("ffffffff"), "num": -2147483647, "numLen": defaultScriptNumLen,
+            #  "minimalEncoding": True, "err": None},
+            # {"serialized": hex_to_bytes("ffffffff7f"), "num": 549755813887, "numLen": 5, "minimalEncoding": True,
+            #  "err": None},
+            # {"serialized": hex_to_bytes("ffffffffff"), "num": -549755813887, "numLen": 5, "minimalEncoding": True,
+            #  "err": None},
+            # {"serialized": hex_to_bytes("ffffffffffffff7f"), "num": 9223372036854775807, "numLen": 8,
+            #  "minimalEncoding": True, "err": None},
+            # {"serialized": hex_to_bytes("ffffffffffffffff"), "num": -9223372036854775807, "numLen": 8,
+            #  "minimalEncoding": True, "err": None},
+            {"serialized": hex_to_bytes("ffffffffffffffff7f"), "num": -1, "numLen": 9, "minimalEncoding": True,
+             "err": None},
+            # {"serialized": hex_to_bytes("ffffffffffffffffff"), "num": 1, "numLen": 9, "minimalEncoding": True,
+            #  "err": None},
+            # {"serialized": hex_to_bytes("ffffffffffffffffff7f"), "num": -1, "numLen": 10, "minimalEncoding": True,
+            #  "err": None},
+            # {"serialized": hex_to_bytes("ffffffffffffffffffff"), "num": 1, "numLen": 10, "minimalEncoding": True,
+            #  "err": None},
+            #
+            # # Minimally encoded values that are out of range for data that
+            # # is interpreted as script numbers with the minimal encoding
+            # # flag set.  Should error and return 0.
+            # {"serialized": hex_to_bytes("0000008000"), "num": 0, "numLen": defaultScriptNumLen, "minimalEncoding": True,
+            #  "err": ErrorCode.ErrNumberTooBig},
+            # {"serialized": hex_to_bytes("0000008080"), "num": 0, "numLen": defaultScriptNumLen, "minimalEncoding": True,
+            #  "err": ErrorCode.ErrNumberTooBig},
+            # {"serialized": hex_to_bytes("0000009000"), "num": 0, "numLen": defaultScriptNumLen, "minimalEncoding": True,
+            #  "err": ErrorCode.ErrNumberTooBig},
+            # {"serialized": hex_to_bytes("0000009080"), "num": 0, "numLen": defaultScriptNumLen, "minimalEncoding": True,
+            #  "err": ErrorCode.ErrNumberTooBig},
+            # {"serialized": hex_to_bytes("ffffffff00"), "num": 0, "numLen": defaultScriptNumLen, "minimalEncoding": True,
+            #  "err": ErrorCode.ErrNumberTooBig},
+            # {"serialized": hex_to_bytes("ffffffff80"), "num": 0, "numLen": defaultScriptNumLen, "minimalEncoding": True,
+            #  "err": ErrorCode.ErrNumberTooBig},
+            # {"serialized": hex_to_bytes("0000000001"), "num": 0, "numLen": defaultScriptNumLen, "minimalEncoding": True,
+            #  "err": ErrorCode.ErrNumberTooBig},
+            # {"serialized": hex_to_bytes("0000000081"), "num": 0, "numLen": defaultScriptNumLen, "minimalEncoding": True,
+            #  "err": ErrorCode.ErrNumberTooBig},
+            # {"serialized": hex_to_bytes("ffffffffffff00"), "num": 0, "numLen": defaultScriptNumLen,
+            #  "minimalEncoding": True, "err": ErrorCode.ErrNumberTooBig},
+            # {"serialized": hex_to_bytes("ffffffffffff80"), "num": 0, "numLen": defaultScriptNumLen,
+            #  "minimalEncoding": True, "err": ErrorCode.ErrNumberTooBig},
+            # {"serialized": hex_to_bytes("ffffffffffffff00"), "num": 0, "numLen": defaultScriptNumLen,
+            #  "minimalEncoding": True, "err": ErrorCode.ErrNumberTooBig},
+            # {"serialized": hex_to_bytes("ffffffffffffff80"), "num": 0, "numLen": defaultScriptNumLen,
+            #  "minimalEncoding": True, "err": ErrorCode.ErrNumberTooBig},
+            # {"serialized": hex_to_bytes("ffffffffffffff7f"), "num": 0, "numLen": defaultScriptNumLen,
+            #  "minimalEncoding": True, "err": ErrorCode.ErrNumberTooBig},
+            # {"serialized": hex_to_bytes("ffffffffffffffff"), "num": 0, "numLen": defaultScriptNumLen,
+            #  "minimalEncoding": True, "err": ErrorCode.ErrNumberTooBig},
+            #
+            # # Non-minimally encoded, but otherwise valid values with
+            # # minimal encoding flag.  Should error and return 0.
+            # {"serialized": hex_to_bytes("00"), "num": 0, "numLen": defaultScriptNumLen, "minimalEncoding": True,
+            #  "err": ErrorCode.ErrMinimalData},  # 0
+            # {"serialized": hex_to_bytes("0100"), "num": 0, "numLen": defaultScriptNumLen, "minimalEncoding": True,
+            #  "err": ErrorCode.ErrMinimalData},  # 1
+            # {"serialized": hex_to_bytes("7f00"), "num": 0, "numLen": defaultScriptNumLen, "minimalEncoding": True,
+            #  "err": ErrorCode.ErrMinimalData},  # 127
+            # {"serialized": hex_to_bytes("800000"), "num": 0, "numLen": defaultScriptNumLen, "minimalEncoding": True,
+            #  "err": ErrorCode.ErrMinimalData},  # 128
+            # {"serialized": hex_to_bytes("810000"), "num": 0, "numLen": defaultScriptNumLen, "minimalEncoding": True,
+            #  "err": ErrorCode.ErrMinimalData},  # 129
+            # {"serialized": hex_to_bytes("000100"), "num": 0, "numLen": defaultScriptNumLen, "minimalEncoding": True,
+            #  "err": ErrorCode.ErrMinimalData},  # 256
+            # {"serialized": hex_to_bytes("ff7f00"), "num": 0, "numLen": defaultScriptNumLen, "minimalEncoding": True,
+            #  "err": ErrorCode.ErrMinimalData},  # 32767
+            # {"serialized": hex_to_bytes("00800000"), "num": 0, "numLen": defaultScriptNumLen, "minimalEncoding": True,
+            #  "err": ErrorCode.ErrMinimalData},  # 32768
+            # {"serialized": hex_to_bytes("ffff0000"), "num": 0, "numLen": defaultScriptNumLen, "minimalEncoding": True,
+            #  "err": ErrorCode.ErrMinimalData},  # 65535
+            # {"serialized": hex_to_bytes("00000800"), "num": 0, "numLen": defaultScriptNumLen, "minimalEncoding": True,
+            #  "err": ErrorCode.ErrMinimalData},  # 524288
+            # {"serialized": hex_to_bytes("00007000"), "num": 0, "numLen": defaultScriptNumLen, "minimalEncoding": True,
+            #  "err": ErrorCode.ErrMinimalData},  # 7340032
+            # {"serialized": hex_to_bytes("0009000100"), "num": 0, "numLen": 5, "minimalEncoding": True,
+            #  "err": ErrorCode.ErrMinimalData},  # 16779520
+            #
+            # # Non-minimally encoded, but otherwise valid values without
+            # # minimal encoding flag.  Should not error and return expected
+            # # integral number.
+            # {"serialized": hex_to_bytes("00"), "num": 0, "numLen": defaultScriptNumLen, "minimalEncoding": False,
+            #  "err": None},
+            # {"serialized": hex_to_bytes("0100"), "num": 1, "numLen": defaultScriptNumLen, "minimalEncoding": False,
+            #  "err": None},
+            # {"serialized": hex_to_bytes("7f00"), "num": 127, "numLen": defaultScriptNumLen, "minimalEncoding": False,
+            #  "err": None},
+            # {"serialized": hex_to_bytes("800000"), "num": 128, "numLen": defaultScriptNumLen, "minimalEncoding": False,
+            #  "err": None},
+            # {"serialized": hex_to_bytes("810000"), "num": 129, "numLen": defaultScriptNumLen, "minimalEncoding": False,
+            #  "err": None},
+            # {"serialized": hex_to_bytes("000100"), "num": 256, "numLen": defaultScriptNumLen, "minimalEncoding": False,
+            #  "err": None},
+            # {"serialized": hex_to_bytes("ff7f00"), "num": 32767, "numLen": defaultScriptNumLen,
+            #  "minimalEncoding": False, "err": None},
+            # {"serialized": hex_to_bytes("00800000"), "num": 32768, "numLen": defaultScriptNumLen,
+            #  "minimalEncoding": False, "err": None},
+            # {"serialized": hex_to_bytes("ffff0000"), "num": 65535, "numLen": defaultScriptNumLen,
+            #  "minimalEncoding": False, "err": None},
+            # {"serialized": hex_to_bytes("00000800"), "num": 524288, "numLen": defaultScriptNumLen,
+            #  "minimalEncoding": False, "err": None},
+            # {"serialized": hex_to_bytes("00007000"), "num": 7340032, "numLen": defaultScriptNumLen,
+            #  "minimalEncoding": False, "err": None},
+            # {"serialized": hex_to_bytes("0009000100"), "num": 16779520, "numLen": 5, "minimalEncoding": False,
+            #  "err": None},
+        ]
+
+        for c in tests:
+            if c['err']:
+                with self.assertRaises(ScriptError) as cm:
+                    make_script_num(c['serialized'], c['minimalEncoding'], c['numLen'])
+                self.assertEqual(cm.exception.c, c['err'])
+            else:
+                self.assertEqual(make_script_num(c['serialized'], c['minimalEncoding'], c['numLen']), c['num'])
+
+
+
+
---
+++ txscript/script_num.py
@@ -0,0 +1,218 @@
+from .error import *
+
+maxInt32 = (1 << 31) - 1
+minInt32 = -1 << 31
+
+# defaultScriptNumLen is the default number of bytes
+# data being interpreted as an integer may be.
+defaultScriptNumLen = 4
+
+
+# scriptNum represents a numeric value used in the scripting engine with
+# special handling to deal with the subtle semantics required by consensus.
+#
+# All numbers are stored on the data and alternate stacks encoded as little
+# endian with a sign bit.  All numeric opcodes such as OP_ADD, OP_SUB,
+# and OP_MUL, are only allowed to operate on 4-byte integers in the range
+# [-2^31 + 1, 2^31 - 1], however the results of numeric operations may overflow
+# and remain valid so long as they are not used as inputs to other numeric
+# operations or otherwise interpreted as an integer.
+#
+# For example, it is possible for OP_ADD to have 2^31 - 1 for its two operands
+# resulting 2^32 - 2, which overflows, but is still pushed to the stack as the
+# result of the addition.  That value can then be used as input to OP_VERIFY
+# which will succeed because the data is being interpreted as a boolean.
+# However, if that same value were to be used as input to another numeric
+# opcode, such as OP_SUB, it must fail.
+#
+# This type handles the aforementioned requirements by storing all numeric
+# operation results as an int64 to handle overflow and provides the Bytes
+# method to get the serialized representation (including values that overflow).
+#
+# Then, whenever data is interpreted as an integer, it is converted to this
+# type by using the makeScriptNum function which will return an error if the
+# number is out of range or not minimally encoded depending on parameters.
+# Since all numeric opcodes involve pulling data from the stack and
+# interpreting it as an integer, it provides the required behavior.
+class ScriptNum:
+    def __init__(self, data=None):
+        self._data = data or 0
+
+    @property
+    def value(self):
+        return self._data
+
+    def __eq__(self, other):
+        if type(other) is int:
+            return self.value == other
+        elif type(other) is ScriptNum:
+            return self.value == other.value
+        else:
+            return False
+
+    # Bytes returns the number serialized as a little endian with a sign bit.
+    #
+    # Example encodings:
+    #       127 -> [0x7f]
+    #      -127 -> [0xff]
+    #       128 -> [0x80 0x00]
+    #      -128 -> [0x80 0x80]
+    #       129 -> [0x81 0x00]
+    #      -129 -> [0x81 0x80]
+    #       256 -> [0x00 0x01]
+    #      -256 -> [0x00 0x81]
+    #     32767 -> [0xff 0x7f]
+    #    -32767 -> [0xff 0xff]
+    #     32768 -> [0x00 0x80 0x00]
+    #    -32768 -> [0x00 0x80 0x80]
+    def bytes(self):
+        n = self.value
+        # Zero encodes as an empty byte slice.
+        if n == 0:
+            return bytes()
+
+            # Take the absolute value and keep track of whether it was originally
+            # negative.
+
+        if n < 0:
+            is_negative = True
+            n = -n
+        else:
+            is_negative = False
+
+        result = bytearray()
+
+        # Encode to little endian.  The maximum number of encoded bytes is 9
+        # (8 bytes for max int64 plus a potential byte for sign extension).
+        while n > 0:
+            result.append(n & 0xff)
+            n >>= 8
+
+        # When the most significant byte already has the high bit set, an
+        # additional high byte is required to indicate whether the number is
+        # negative or positive.  The additional byte is removed when converting
+        # back to an integral and its high bit is used to denote the sign.
+        #
+        # Otherwise, when the most significant byte does not already have the
+        # high bit set, use it to indicate the value is negative, if needed.
+        if result[-1] & 0x80 != 0:
+
+            if is_negative:
+                extra_byte = 0x80
+            else:
+                extra_byte = 0x00
+
+            result.append(extra_byte)
+        elif is_negative:
+            result[-1] |= 0x80
+
+        return bytes(result)
+
+    # Int32 returns the script number clamped to a valid int32.  That is to say
+    # when the script number is higher than the max allowed int32, the max int32
+    # value is returned and vice versa for the minimum value.  Note that this
+    # behavior is different from a simple int32 cast because that truncates
+    # and the consensus rules dictate numbers which are directly cast to ints
+    # provide this behavior.
+    #
+    # In practice, for most opcodes, the number should never be out of range since
+    # it will have been created with makeScriptNum using the defaultScriptLen
+    # value, which rejects them.  In case something in the future ends up calling
+    # this function against the result of some arithmetic, which IS allowed to be
+    # out of range before being reinterpreted as an integer, this will provide the
+    # correct behavior.
+    def int32(self):
+        if self.value > maxInt32:
+            return maxInt32
+
+        if self.value < minInt32:
+            return minInt32
+
+        return self.value
+
+
+# makeScriptNum interprets the passed serialized bytes as an encoded integer
+# and returns the result as a script number.
+#
+# Since the consensus rules dictate that serialized bytes interpreted as ints
+# are only allowed to be in the range determined by a maximum number of bytes,
+# on a per opcode basis, an error will be returned when the provided bytes
+# would result in a number outside of that range.  In particular, the range for
+# the vast majority of opcodes dealing with numeric values are limited to 4
+# bytes and therefore will pass that value to this function resulting in an
+# allowed range of [-2^31 + 1, 2^31 - 1].
+#
+# The requireMinimal flag causes an error to be returned if additional checks
+# on the encoding determine it is not represented with the smallest possible
+# number of bytes or is the negative 0 encoding, [0x80].  For example, consider
+# the number 127.  It could be encoded as [0x7f], [0x7f 0x00],
+# [0x7f 0x00 0x00 ...], etc.  All forms except [0x7f] will return an error with
+# requireMinimal enabled.
+#
+# The scriptNumLen is the maximum number of bytes the encoded value can be
+# before an ErrStackNumberTooBig is returned.  This effectively limits the
+# range of allowed values.
+# WARNING:  Great care should be taken if passing a value larger than
+# defaultScriptNumLen, which could lead to addition and multiplication
+# overflows.
+#
+# See the Bytes function documentation for example encodings.
+def make_script_num(v: bytes, require_minial: bool, script_num_len: int):
+    if len(v) > script_num_len:
+        desc = "numeric value encoded as {} is {} bytes which exceeds the max allowed of {}".format(str(v), len(v),
+                                                                                                    script_num_len)  # TODO check the format output
+        raise ScriptError(ErrorCode.ErrNumberTooBig, desc=desc)
+
+    if require_minial:
+        check_minimal_data_encoding(v)
+
+    if len(v) == 0:
+        return 0
+
+    result = 0
+
+    # Decode from little endian.
+    for i in range(len(v)):
+        result |= (v[i] << (8 * i))
+        print(i)
+        print(result)
+
+
+    # When the most significant byte of the input bytes has the sign bit
+    # set, the result is negative.  So, remove the sign bit from the result
+    # and make it negative.
+    if v[-1] & 0x80 != 0:
+        # The maximum length of v has already been determined to be 4
+        # above, so uint8 is enough to cover the max possible shift
+        # value of 24.
+        result &= ~(0x80 << 8 * (len(v) - 1))  # TOCHECK why
+
+        return ScriptNum(-result)
+
+    return ScriptNum(result)
+
+
+# checkMinimalDataEncoding returns whether or not the passed byte array adheres
+# to the minimal encoding requirements.
+def check_minimal_data_encoding(v: bytes):
+    if len(v) == 0:
+        return
+
+    # Check that the number is encoded with the minimum possible
+    # number of bytes.
+    #
+    # If the most-significant-byte - excluding the sign bit - is zero
+    # then we're not minimal.  Note how this test also rejects the
+    # negative-zero encoding, [0x80].
+
+    if v[-1] & 0x7f == 0:  # 0x7f:   01111111
+        # One exception: if there's more than one byte and the most
+        # significant bit of the second-most-significant-byte is set
+        # it would conflict with the sign bit.  An example of this case
+        # is +-255, which encode to 0xff00 and 0xff80 respectively.
+        # (little-endian).
+        if len(v) == 1 or (v[-2] & 0x80 == 0):  # 0x80:   10000000
+            desc = "numeric value encoded as {} is not minimally encoded".format(str(v))  # TODO check the format output
+            raise ScriptError(ErrorCode.ErrMinimalData, desc=desc)
+
+    return
