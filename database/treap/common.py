8f09b3c9024b7d0ddade591aa968b80a35e40c2b
Nisen <imnisen@gmail.com>
2018-10-27 09:06:35 +0800

    Add mutable and immutable treap

--- database/treap/common.py
+++ database/treap/common.py
@@ -1,16 +1,36 @@
+# nodeFieldsSize is the size the fields of each node takes excluding
+# the contents of the key and value.  It assumes 64-bit pointers so
+# technically it is smaller on 32-bit platforms, but overestimating the
+# size in that case is acceptable since it avoids the need to import
+# unsafe.  It consists of 24-bytes for each key and value + 8 bytes for
+# each of the priority, left, and right fields (24*2 + 8*3).
+nodeFieldsSize = 72  # TOCHECK , why not 8*3 = 24?
+
 class TreapNode:
-    def __init__(self, key, value=None, priority=None, parent=None, left=None, right=None):
+    def __init__(self, key, value, priority, left=None, right=None):
         self.key = key
         self.value = value
         self.priority = priority
-        self.parent = parent
         self.left = left
         self.right = right
 
     def __repr__(self):
         return str((self.key, self.value, self.priority))
 
+    def node_size(self):
+        return nodeFieldsSize + len(self.key), len(self.value)
 
 
+class ParentStack(list):
+
+    def at(self, n):
+
+        index = len(self) - 1 - n
+
+        if index < 0:
+            return None
+        if index > len(self) - 1:
+            raise IndexError
+        return self[index]
 
 
--- database/treap/immutable.py
+++ database/treap/immutable.py
@@ -10,8 +10,18 @@ def get_random_int():
     return random_fn.randrange(pyutil.MaxInt64)
 
 
+def clone_treap_node(node):
+    return TreapNode(
+        key=node.key,
+        value=node.value,
+        priority=node.priority,
+        left=node.left,
+        right=node.right,
+    )
+
+
 # You can refer to https://blog.csdn.net/yang_yulei/article/details/46005845 for treap
-class Mutable:
+class Immutable:
     def __init__(self, root=None, count=None, total_size=None):
         self.root = root
         self.count = count or 0
@@ -43,17 +53,27 @@ class Mutable:
         parent = None
         parent_stack = ParentStack()
         while node is not None:
+            # copy the node following the find path
+            node_copy = clone_treap_node(node)
+
+            # change the parent point to new copy
+            if parent is not None:
+                if parent.left == node:
+                    parent.left = node_copy
+                else:
+                    parent.right = node_copy
+
             result = self._key_compare(key, node)
             if result > 0:
-                parent = node
+                parent = node_copy
                 parent_stack.append(parent)
                 node = node.right
             elif result < 0:
-                parent = node
+                parent = node_copy
                 parent_stack.append(parent)
                 node = node.left
             else:
-                return node, parent, parent_stack
+                return node_copy, parent, parent_stack
         return None, parent, parent_stack
 
     def get(self, key: bytes):
@@ -67,18 +87,10 @@ class Mutable:
         node, _, _ = self._locator_key(key)
         return node is not None
 
-    def _relink_grand_parent(self, node, parent, grandparent):
-        if grandparent is None:
-            self.root = node
+    @staticmethod
+    def _check_priority_and_rotate_return_root(node, parent_stack):
 
-        if grandparent.left == parent:
-            grandparent.left = node
-        else:
-            grandparent.right = node
-        return
-
-    def _check_priority_and_rotate(self, node, parent_stack):
-
+        new_root = parent_stack.at(len(parent_stack) - 1)
         while len(parent_stack) > 0:
             parent = parent_stack.pop()
 
@@ -96,7 +108,13 @@ class Mutable:
 
             # Relink grandparent
             grandparent = parent_stack.at(0)
-            self._relink_grand_parent(node, parent, grandparent)
+            if grandparent is None:
+                new_root = node
+            if grandparent.left == parent:
+                grandparent.left = node
+            else:
+                grandparent.right = node
+        return new_root
 
     def _attach_node_to_parent(self, node, parent):
         result = self._key_compare(node.value, parent.value)
@@ -112,40 +130,38 @@ class Mutable:
             value = bytes()
 
         # Make new node if it is a empty treap
+        # Then make the new Immutable and return
         if self.root is None:
             node = TreapNode(key, value, get_random_int())
-            self.count = 1
-            self.total_size = node.node_size()
-            self.root = node
-            return
+            return Immutable(root=node, count=1, total_size=node.node_size())
 
         # locator the key
         node, parent, parent_stack = self._locator_key(key)
 
         # We put the key that already exsits
         if node is not None:
-            self.total_size -= len(node.value)
+            new_root = parent_stack[0]
+            new_total_size = self.total_size - len(node.value) + len(value)
             node.value = value
-            self.total_size += len(node.value)
-            return
+            return Immutable(root=new_root, count=self.count, total_size=new_total_size)
 
         # Make a new node
         node = TreapNode(key, value, get_random_int())
-        self.count += 1
-        self.total_size += node.node_size()
+        new_count = self.count + 1
+        new_total_size = self.total_size + node.node_size()
 
         # attach to it's parent
         self._attach_node_to_parent(node, parent)
 
         # rotate node and it's parent if they don't satisfy heap rule
-        self._check_priority_and_rotate(node, parent_stack)
+        new_root = self._check_priority_and_rotate_return_root(node, parent_stack)
 
-        return
+        return Immutable(root=new_root, count=new_count, total_size=new_total_size)
 
     def delete(self, key):
 
         # locator the key
-        node, parent, _ = self._locator_key(key)
+        node, parent, parent_stack = self._locator_key(key)
 
         # when treap don't has key
         if node is None:
@@ -153,13 +169,11 @@ class Mutable:
 
         # when treap only has single node
         if parent is None and node.left is None and node.right is None:
-            self.root = None
-            self.count = 0
-            self.total_size = 0
-            return
+            return Immutable(root=None, count=0, total_size=0)
 
         # rotate to make node become leaf
         # loop while node has child
+        new_root = parent_stack.at(len(parent_stack) - 1)
         while node.left is not None or node.right is not None:
             # choose child with higher priority
             if node.left is None:
@@ -175,6 +189,8 @@ class Mutable:
                 child = node.right
                 is_left = False
 
+            child = clone_treap_node(child)
+
             if is_left:
                 # child on left branch, do right rotation
                 node.left = child.right
@@ -183,9 +199,13 @@ class Mutable:
                 # child on left branch, do right rotation
                 node.right = child.left
                 child.left = node
-            self._relink_grand_parent(child, node, parent)
 
-            parent = child
+            if parent is None:
+                new_root = child
+            elif parent.left == node:
+                parent.left = child
+            else:
+                parent.right = child
 
         # pop node from it's parent
         if parent.right == node:
@@ -194,9 +214,9 @@ class Mutable:
             parent.left = None
 
         # do count stuff
-        self.count -= 1
-        self.total_size -= node.node_size()
-        return
+        new_count = self.count - 1
+        new_total_size = self.total_size - node.node_size()
+        return Immutable(root=new_root, count=new_count, total_size=new_total_size)
 
     # TOCHANGE very golang style
     # But now, make the inteface same. After I understand fully, change it better
@@ -225,10 +245,4 @@ class Mutable:
                 parent_stack.append(node)
                 node = node.left
 
-        return
-
-    def reset(self):
-        self.count = 0
-        self.total_size = 0
-        self.root = None
         return
