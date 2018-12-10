064f2da9eab8fb988521e5de1f4802b2321fd2d4
nisen <imnisen@gmail.com>
2018-12-10 14:06:07 +0800

    Add constants

--- blockchain/weight.py
+++ blockchain/weight.py
@@ -1,10 +1,36 @@
 import btcutil
 from .utxo_viewpoint import *
 
+# MaxBlockWeight defines the maximum block weight, where "block
+# weight" is interpreted as defined in BIP0141. A block's weight is
+# calculated as the sum of the of bytes in the existing transactions
+# and header, plus the weight of each byte within a transaction. The
+# weight of a "base" byte is 4, while the weight of a witness byte is
+# 1. As a result, for a block to be valid, the BlockWeight MUST be
+# less than, or equal to MaxBlockWeight.
+MaxBlockWeight = 4000000
+
+# MaxBlockBaseSize is the maximum number of bytes within a block
+# which can be allocated to non-witness data.
+MaxBlockBaseSize = 1000000
+
+# MaxBlockSigOpsCost is the maximum number of signature operations
+# allowed for a block. It is calculated via a weighted algorithm which
+# weights segregated witness sig ops lower than regular sig ops.
+MaxBlockSigOpsCost = 80000
+
 # WitnessScaleFactor determines the level of "discount" witness data
 # receives compared to "base" data. A scale factor of 4, denotes that
 # witness data is 1/4 as cheap as regular non-witness data.
 WitnessScaleFactor = 4
+
+# MinTxOutputWeight is the minimum possible weight for a transaction
+# output.
+MinTxOutputWeight = WitnessScaleFactor * wire.MinTxOutPayload
+
+# MaxOutputsPerBlock is the maximum number of transaction outputs there
+# can be in a block of max weight size.
+MaxOutputsPerBlock = MaxBlockWeight / MinTxOutputWeight
 
 
 # GetBlockWeight computes the value of the weight metric for a given block.
