e12c717777dd0bda3dfc993d1078275be69fb82e
nisen <imnisen@gmail.com>
2018-12-12 09:57:03 +0800

    Add get_sig_op_cost

--- blockchain/weight.py
+++ blockchain/weight.py
@@ -61,7 +61,6 @@ def get_transaction_weight(tx: btcutil.Tx) -> int:
     return (base_size * (WitnessScaleFactor - 1)) + total_size
 
 
-# TODO
 # GetSigOpCost returns the unified sig op cost for the passed transaction
 # respecting current active soft-forks which modified sig op cost counting.
 # The unified sig op cost for a transaction is computed as the sum of: the
@@ -70,4 +69,28 @@ def get_transaction_weight(tx: btcutil.Tx) -> int:
 # unscaled sig op count for any inputs spending witness programs.
 def get_sig_op_cost(tx: btcutil.Tx, is_coin_base_p: bool, utxo_view: UtxoViewpoint,
                     bip16_p: bool, seg_wit_p: bool) -> int:
-    pass
+    num_sig_ops = count_sig_ops(tx) * WitnessScaleFactor
+    if bip16_p:
+        num_p2sh_sig_ops = count_p2sh_sig_ops(tx, is_coin_base_p, utxo_view)
+        num_sig_ops += num_p2sh_sig_ops * WitnessScaleFactor
+
+    if seg_wit_p and not is_coin_base_p:
+        msg_tx = tx.get_msg_tx()
+        for i, tx_in in enumerate(msg_tx.tx_ins):
+            # Ensure the referenced output is available and hasn't
+            # already been spent.
+            utxo = utxo_view.lookup_entry(tx_in.previous_out_point)
+            if utxo is None or utxo.is_spent():
+                msg = ("output %s referenced from " +
+                       "transaction %s:%d either does not " +
+                       "exist or has already been spent") % (
+                          tx_in.previous_out_point, tx.hash(), i
+                      )
+
+                raise RuleError(ErrorCode.ErrMissingTxOut, msg)
+
+            witness = tx_in.witness
+            sig_script = tx_in.signature_script
+            pk_script = utxo.pk_script
+            num_sig_ops += txscript.get_witness_sig_op_count(sig_script, pk_script, witness)
+    return num_sig_ops
