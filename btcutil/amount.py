7388b97e32eaceb3ff9bff56408be42adea80bee
nisen <imnisen@gmail.com>
2018-12-18 15:16:51 +0800

    Add btcutil/amount

---
+++ btcutil/amount.py
@@ -0,0 +1,6 @@
+# Amount represents the base bitcoin monetary unit (colloquially referred
+# to as a `Satoshi').  A single Amount is equal to 1e-8 of a bitcoin.
+class Amount(int):
+    pass
+
+# TODO
