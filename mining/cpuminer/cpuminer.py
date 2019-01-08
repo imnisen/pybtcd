f3627a167a7b551fe12eb537680ddb810b0ef480
nisen <imnisen@gmail.com>
2019-01-08 18:15:34 +0800

    Use aiochan to do mining work concurrent

--- mining/cpuminer/cpuminer.py
+++ mining/cpuminer/cpuminer.py
@@ -3,7 +3,26 @@ import mining
 import btcutil
 import wire
 import chainhash
+import pyutil
 from typing import Callable
+import aiochan as ac
+import asyncio
+import random
+import time
+import logging
+
+logger = logging.getLogger(__name__)
+
+# hpsUpdateSecs is the number of seconds to wait in between each
+# update to the hashes per second monitor.
+hpsUpdateSecs = 10
+
+# hashUpdateSec is the number of seconds each worker waits in between
+# notifying the speed monitor with how many hashes have been completed
+# while they are actively searching for a solution.  This is done to
+# reduce the amount of syncs between the workers that must be done to
+# keep track of the hashes per second.
+hashUpdateSecs = 15
 
 
 # Config is a descriptor containing the cpu miner configuration.
@@ -62,18 +81,84 @@ class Config:
 # function, but the default is based on the number of processor cores in the
 # system which is typically sufficient.
 class CPUMiner:
-    def __init__(self, ):
-        pass  # TODO
+    def __init__(self, lock=None, g: mining.BlkTmplGenerator = None, cfg: Config = None, num_workers=None, started=None,
+                 discrete_mining=None,
+                 submit_block_lock: pyutil.Lock = None, update_num_workers=None, query_hashes_per_sec=None,
+                 update_hashes=None, speed_monitor_quit=None, quit=None):
+        self._lock = lock or pyutil.Lock()
+        self.g = g
+        self.cfg = cfg
+        self.num_workers = num_workers
+        self.started = started
+        self.discrete_mining = discrete_mining
+        self.submit_block_lock = submit_block_lock or pyutil.Lock()
+        self.update_num_workers = update_num_workers
+        self.query_hashes_per_sec = query_hashes_per_sec
+        self.update_hashes = update_hashes
+
+        self.speed_monitor_quit = speed_monitor_quit
+        self.quit = quit
+
+        # # TODO  maybe invent a waitgroup here?
+        # wg                sync.WaitGroup
+        # workerWg          sync.WaitGroup
+
+    def lock(self):
+        self._lock.acquire()
+
+    def unlock(self):
+        self._lock.release()
 
     # speedMonitor handles tracking the number of hashes per second the mining
     # process is performing.  It must be run as a goroutine.
-    def _speed_monitor(self):
-        pass
+    async def _speed_monitor(self):
+        logger.info("CPU miner speed monitor started")
+
+        ticker = ac.tick_tock(hpsUpdateSecs)
+
+        hashes_per_sec = 0
+        total_hashes = 0
+
+        try:
+            while True:
+                result, c = await ac.select(self.update_hashes, ticker, (self.query_hashes_per_sec, hashes_per_sec),
+                                            self.speed_monitor_quit)
+
+                # Periodic updates from the workers with how many hashes they
+                # have performed.
+                if c is self.update_hashes:
+                    num_hashes = result
+                    total_hashes += num_hashes
+
+                # Time to update the hashes per second.
+                elif c is ticker:
+                    cur_hashes_per_sec = total_hashes / hpsUpdateSecs
+                    if hashes_per_sec == 0:
+                        hashes_per_sec = cur_hashes_per_sec
+                        hashes_per_sec = (hashes_per_sec + cur_hashes_per_sec) / 2
+                        total_hashes = 0
+                        if hashes_per_sec != 0:
+                            logger.info("Hash speed: %6.0f kilohashes/s" % hashes_per_sec / 1000)
+
+                # Request for the number of hashes per second.
+                elif c is self.query_hashes_per_sec:
+                    # Nothing to do.
+                    pass
+
+                else:
+                    break
+
+            # TODO m.wg.Done()
+            logger.info("CPU miner speed monitor done")
+            return
+
+        finally:
+            ticker.close()
 
     # submitBlock submits the passed block to network after ensuring it passes all
     # of the consensus validation rules.
     def _submit_block(self, block: btcutil.Block):
-        pass
+        pass  # TODO
 
     # solveBlock attempts to find some combination of a nonce, extra nonce, and
     # current timestamp which makes the passed block hash to a value less than the
@@ -85,7 +170,7 @@ class CPUMiner:
     # stale block such as a new block showing up or periodically when there are
     # new transactions and enough time has elapsed without finding a solution.
     def _solve_block(self, msg_block: wire.MsgBlock, block_height: int, ticker, quit):
-        pass
+        pass  # TODO
 
     # generateBlocks is a worker that is controlled by the miningWorkerController.
     # It is self contained in that it creates block templates and attempts to solve
@@ -94,16 +179,122 @@ class CPUMiner:
     # is submitted.
     #
     # It must be run as a goroutine.
-    def _generate_blocks(self, quit):
-        pass
+    async def _generate_blocks(self, quit):
+        logger.info("Starting generate blocks worker")
+
+        # Start a ticker which is used to signal checks for stale work and
+        # updates to the speed monitor.
+        ticker = ac.tick_tock(hashUpdateSecs)
+        try:
+            while True:
+                # Quit when the miner is stopped.
+                result, c = await ac.select(quit, default="giveup")
+                if c is quit:
+                    break
+                else:
+                    # Non-blocking select to fall through
+                    pass
+
+                # Wait until there is a connection to at least one other peer
+                # since there is no way to relay a found block or receive
+                # transactions to work on when there are no connected peers.
+                if self.cfg.connected_count() == 0:
+                    time.sleep(1)  # TODO TOCHECK async?
+                    continue
+
+                # No point in searching for a solution before the chain is
+                # synced.  Also, grab the same lock as used for block
+                # submission, since the current block will be changing and
+                # this would otherwise end up building a new block template on
+                # a block that is in the process of becoming stale.
+                self.submit_block_lock.lock()
+                cur_height = self.g.best_snapshot().height
+                if cur_height != 0 and not self.cfg.is_current():
+                    self.submit_block_lock.unlock()
+                    time.sleep(1)  # TODO TOCHECK async?
+                    continue
+
+                # Choose a payment address at random.
+                pay_to_addr = random.choices(self.cfg.mining_addrs)
+
+                # Create a new block template using the available transactions
+                # in the memory pool as a source of transactions to potentially
+                # include in the block.
+                try:
+                    template = self.g.new_block_template(pay_to_addr)
+                except Exception as e:
+                    msg = "Failed to create new block template: %s" % e
+                    logger.warning(msg)
+                    continue
+                finally:
+                    self.submit_block_lock.unlock()
+
+                # Attempt to solve the block.  The function will exit early
+                # with false when conditions that trigger a stale block, so
+                # a new block template can be generated.  When the return is
+                # true a solution was found, so submit the solved block.
+                if self._solve_block(template.block, cur_height + 1, ticker, quit):
+                    block = btcutil.Block.from_msg_block(template.block)
+                    self._submit_block(block)
+
+            # TODO m.workerWg.Done()
+            logger.info("Generate blocks worker done")
+
+        finally:
+            ticker.close()
 
     # miningWorkerController launches the worker goroutines that are used to
     # generate block templates and solve them.  It also provides the ability to
     # dynamically adjust the number of running worker goroutines.
     #
     # It must be run as a goroutine.
-    def _mining_worker_controller(self):
-        pass
+    async def _mining_worker_controller(self):
+        # launchWorkers groups common code to launch a specified number of
+        # workers for generating blocks.
+        running_workers = []
+
+        def launch_workers(num_workers: int):
+            for _ in range(num_workers):
+                quit = ac.Chan()
+                running_workers.append(quit)
+
+                # m.workerWg.Add(1)
+
+                ac.go(self._generate_blocks(quit))
+
+        # Launch the current number of workers by default.
+        launch_workers(self.num_workers)
+
+        while True:
+            result, c = await ac.select(self.update_num_workers, self.quit)
+
+            # Update the number of running workers.
+            if c is self.update_num_workers:
+                # No change
+                num_running = len(running_workers)
+                if self.num_workers == num_running:
+                    continue
+
+                # Add new workers.
+                if self.num_workers > num_running:
+                    launch_workers(self.num_workers - num_running)
+                    continue
+
+                # Signal the most recently created goroutines to exit.
+                for _ in range(num_running - self.num_workers):
+                    quit_chan = running_workers.pop()
+                    quit_chan.close()
+            else:
+                for quit_chan in running_workers:
+                    quit_chan.close()
+                break
+
+        # Wait until all workers shut down to stop the speed monitor since
+        # they rely on being able to send updates to it.
+        # TODO m.workerWg.Wait()
+        self.speed_monitor_quit.close()
+        # TODO m.wg.Done()
+        return
 
     # Start begins the CPU mining process as well as the speed monitor used to
     # track hashing metrics.  Calling this function when the CPU miner has
@@ -111,7 +302,25 @@ class CPUMiner:
     #
     # This function is safe for concurrent access.
     def start(self):
-        pass
+        with self._lock:
+            # Nothing to do if the miner is already running or if running in
+            # discrete mode (using GenerateNBlocks).
+            if self.started or self.discrete_mining:
+                return
+
+            self.quit = ac.Chan()
+            self.speed_monitor_quit = ac.Chan()
+
+            # TODO m.wg.Add(2)
+
+            ac.go(self._speed_monitor())
+            ac.go(self._mining_worker_controller())
+
+            self.started = True
+
+            logger.info("CPU miner started")
+
+            return
 
     # Stop gracefully stops the mining process by signalling all workers, and the
     # speed monitor to quit.  Calling this function when the CPU miner has not
@@ -119,21 +328,41 @@ class CPUMiner:
     #
     # This function is safe for concurrent access.
     def stop(self):
-        pass
+        with self._lock:
+            # Nothing to do if the miner is not currently running or if running in
+            # discrete mode (using GenerateNBlocks).
+            if not self.started or self.discrete_mining:
+                return
+
+            self.quit.close()
+
+            # TODO m.wg.Wait()
+
+            self.started = False
+
+            logger.info("CPU miner stopped")
+
+            return
 
     # IsMining returns whether or not the CPU miner has been started and is
     # therefore currenting mining.
     #
     # This function is safe for concurrent access.
     def is_mining(self) -> bool:
-        pass
+        with self._lock:
+            return self.started
 
     # HashesPerSecond returns the number of hashes per second the mining process
     # is performing.  0 is returned if the miner is not currently running.
     #
     # This function is safe for concurrent access.
     def hashes_per_second(self) -> float:
-        pass
+        with self._lock:
+            # Nothing to do if the miner is not currently running.
+            if not self.started:
+                return 0
+
+            return self.query_hashes_per_sec.get()  # TODO coroutine?
 
     # SetNumWorkers sets the number of workers to create which solve blocks.  Any
     # negative values will cause a default number of workers to be used which is
@@ -147,8 +376,9 @@ class CPUMiner:
     # NumWorkers returns the number of workers which are running to solve blocks.
     #
     # This function is safe for concurrent access.
-    def num_workers(self) -> int:
-        pass
+    def get_num_workers(self) -> int:
+        with self._lock:
+            return self.num_workers
 
     # GenerateNBlocks generates the requested number of blocks. It is self
     # contained in that it creates block templates and attempts to solve them while
