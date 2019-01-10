6ea1f1563bc34e32828bf3285f43b8b8d49a8f40
nisen <imnisen@gmail.com>
2019-01-10 16:09:08 +0800

    Refactor lock to add lock and unlock method

--- pyutil/lock.py
+++ pyutil/lock.py
@@ -1,9 +1,24 @@
-from threading import Lock as BaseLock
+import threading
 
 
-class Lock(BaseLock):
-    def lock(self, *args, **kwargs):
-        return self.acquire(*args, **kwargs)
+class Lock:
+    def __init__(self):
+        self._lock = threading.Lock()
+
+    def acquire(self):
+        self._lock.acquire()
+
+    def release(self):
+        self._lock.release()
+
+    def lock(self):
+        self._lock.acquire()
 
     def unlock(self):
-        return self.release()
+        self._lock.release()
+
+    def __enter__(self):
+        self.acquire()
+
+    def __exit__(self, type, value, traceback):
+        self.release()
