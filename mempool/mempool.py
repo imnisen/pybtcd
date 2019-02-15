e42fb602a77ecb9e3f25dad967cf7c78f8aed907
nisen <imnisen@gmail.com>
2019-02-15 17:49:07 +0800

    Add mempool basic struct

---
+++ mempool/mempool.py
@@ -0,0 +1,183 @@
+import chaincfg
+import typing
+import txscript
+import btcutil
+import mining
+import pyutil
+
+
+# Tag represents an identifier to use for tagging orphan transactions.  The
+# caller may choose any scheme it desires, however it is common to use peer IDs
+# so that orphans can be identified by which peer first relayed them.
+class Tag(int):
+    pass
+
+
+# Config is a descriptor containing the memory pool configuration.
+class Config:
+    def __init__(self, policy=None,  # TODO
+                 chain_params: chaincfg.Params = None,
+                 fetch_utxo_view: typing.Callable = None,
+                 best_height: typing.Callable = None,
+                 median_time_past: typing.Callable = None,
+                 cacl_sequence_lock: typing.Callable = None,
+                 is_deployment_active: typing.Callable = None,
+                 sig_cache: txscript.SigCache = None,
+                 hash_cache: txscript.HashCache = None,
+                 addr_index=None,  # TODO
+                 fee_estimator=None):  # TODO
+
+        # Policy defines the various mempool configuration options related
+        # to policy.
+        self.policy = policy
+
+        # ChainParams identifies which chain parameters the txpool is
+        # associated with.
+        self.chain_params = chain_params
+
+        # FetchUtxoView defines the function to use to fetch unspent
+        # transaction output information.
+        self.fetch_utxo_view = fetch_utxo_view
+
+        # BestHeight defines the function to use to access the block height of
+        # the current best chain.
+        self.best_height = best_height
+
+        # MedianTimePast defines the function to use in order to access the
+        # median time past calculated from the point-of-view of the current
+        # chain tip within the best chain.
+        self.median_time_past = median_time_past
+
+        # CalcSequenceLock defines the function to use in order to generate
+        # the current sequence lock for the given transaction using the passed
+        # utxo view.
+        self.cacl_sequence_lock = cacl_sequence_lock
+
+        # IsDeploymentActive returns true if the target deploymentID is
+        # active, and false otherwise. The mempool uses this function to gauge
+        # if transactions using new to be soft-forked rules should be allowed
+        # into the mempool or not.
+        self.is_deployment_active = is_deployment_active
+
+        # SigCache defines a signature cache to use.
+        self.sig_cache = sig_cache
+
+        # HashCache defines the transaction hash mid-state cache to use.
+        self.hash_cache = hash_cache
+
+        # AddrIndex defines the optional address index instance to use for
+        # indexing the unconfirmed transactions in the memory pool.
+        # This can be nil if the address index is not enabled.
+        self.addr_index = addr_index
+
+        # FeeEstimatator provides a feeEstimator. If it is not nil, the mempool
+        # records all new transactions it observes into the feeEstimator.
+        self.fee_estimator = fee_estimator
+
+
+class Policy:
+    def __init__(self, max_tx_version: int = None,
+                 disable_relay_priority: bool = None,
+                 accept_non_std: bool = None,
+                 free_tx_relay_limit: float = None,
+                 max_orphan_txs: int = None,
+                 max_orphan_tx_size: int = None,
+                 max_sig_op_cost_per_tx: int = None,
+                 min_relay_tx_fee: btcutil.Amount = None):
+        # MaxTxVersion is the transaction version that the mempool should
+        # accept.  All transactions above this version are rejected as
+        # non-standard.
+        self.max_tx_version = max_tx_version or 0
+
+        # DisableRelayPriority defines whether to relay free or low-fee
+        # transactions that do not have enough priority to be relayed.
+
+        self.disable_relay_priority = disable_relay_priority or False
+
+        # AcceptNonStd defines whether to accept non-standard transactions. If
+        # true, non-standard transactions will be accepted into the mempool.
+        # Otherwise, all non-standard transactions will be rejected.
+
+        self.accept_non_std = accept_non_std or False
+
+        # FreeTxRelayLimit defines the given amount in thousands of bytes
+        # per minute that transactions with no fee are rate limited to.
+
+        self.free_tx_relay_limit = free_tx_relay_limit or 0.0
+
+        # MaxOrphanTxs is the maximum number of orphan transactions
+        # that can be queued.
+        self.max_orphan_txs = max_orphan_txs or 0
+
+        # MaxOrphanTxSize is the maximum size allowed for orphan transactions.
+        # This helps prevent memory exhaustion attacks from sending a lot of
+        # of big orphans.
+        self.max_orphan_tx_size = max_orphan_tx_size or 0
+
+        # MaxSigOpCostPerTx is the cumulative maximum cost of all the signature
+        # operations in a single transaction we will relay or mine.  It is a
+        # fraction of the max signature operations for a block.
+
+        self.max_sig_op_cost_per_tx = max_sig_op_cost_per_tx or 0
+
+        # MinRelayTxFee defines the minimum transaction fee in BTC/kB to be
+        # considered a non-zero fee.
+        self.min_relay_tx_fee = min_relay_tx_fee or btcutil.Amount(0)
+
+
+# TxDesc is a descriptor containing a transaction in the mempool along with
+# additional metadata.
+class TxDesc(mining.TxDesc):
+    def __init__(self, starting_priority: float = None):
+        # StartingPriority is the priority of the transaction when it was added
+        # to the pool.
+        self.starting_priority = starting_priority or 0.0
+
+
+# orphanTx is normal transaction that references an ancestor transaction
+# that is not yet available.  It also contains additional information related
+# to it such as an expiration time to help prevent caching the orphan forever.
+class OrphanTx:
+    def __init__(self, tx: btcutil.Tx = None,
+                 tag: Tag = None,
+                 expiration: int = None):
+        self.tx = tx
+        self.tag = tag
+        self.expiration = expiration
+
+
+# TxPool is used as a source of transactions that need to be mined into blocks
+# and relayed to other peers.  It is safe for concurrent access from multiple
+# peers.
+class TxPool:
+    def __init__(self, last_updated: int = None,
+                 mtx: pyutil.RWLock = None,
+                 cfg: Config = None,
+                 pool: dict = None,
+                 orphans: dict = None,
+                 orphans_by_prev: dict = None,
+                 outpoints: dict = None,
+                 penny_total: float = None,
+                 last_penny_unix: int = None,
+                 next_expire_scan: int = None):
+        # The following variables must only be used atomically.
+
+        # last time pool was updated
+        self.last_updated = last_updated
+        self.mtx = mtx
+        self.cfg = cfg
+        self.pool = pool
+        self.orphans = orphans
+        self.orphans_by_prev = orphans_by_prev
+        self.outpoints = outpoints
+
+        # exponentially decaying total for penny spends.
+        self.penny_total = penny_total
+        # unix time of last ``penny spend''
+        self.last_penny_unix = last_penny_unix
+
+        # nextExpireScan is the time after which the orphan pool will be
+        # scanned in order to evict orphans.  This is NOT a hard deadline as
+        # the scan will only run when an orphan is added to the pool as opposed
+        # to on an unconditional timer.
+        self.next_expire_scan = next_expire_scan
