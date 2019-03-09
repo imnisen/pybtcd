4c3565b142844e9a328a306e86888f35a73356cc
nisen <imnisen@gmail.com>
2019-03-09 21:48:33 +0800

    Add AddrIndex index_pk_script method

--- blockchain/indexers/addr_index.py
+++ blockchain/indexers/addr_index.py
@@ -1,8 +1,8 @@
 import database
 import chaincfg
 import pyutil
+import txscript
 from .common import *
-
 
 # addrIndexName is the human-readable name for the index.
 addrIndexName = "address index"
@@ -10,6 +10,15 @@ addrIndexName = "address index"
 # addrIndexKey is the key of the address index and the db bucket used
 # to house it.
 addrIndexKey = b"txbyaddridx"
+
+
+# writeIndexData represents the address index data to be written for one block.
+# It consists of the address mapped to an ordered list of the transactions
+# that involve the address in block.  It is ordered so the transactions can be
+# stored in the order they appear in the block.
+class WriteIndexData(dict):
+    pass
+
 
 class AddrIndex(Indexer, NeedsInputser):
     def __init__(self, db: database.DB, chain_params: chaincfg.Params,
@@ -50,13 +59,13 @@ class AddrIndex(Indexer, NeedsInputser):
     # This is part of the Indexer interface.
     def init(self):
         return
-    
+
     # Key returns the database key to use for the index as a byte slice.
     #
     # This is part of the Indexer interface.
     def key(self) -> bytes:
         raise addrIndexKey
-    
+
     # Name returns the human-readable name of the index.
     #
     # This is part of the Indexer interface.
@@ -72,6 +81,42 @@ class AddrIndex(Indexer, NeedsInputser):
         db_tx.metadata().create_bucket(addrIndexKey)
         return
 
-    # TODO writeIndexData and indexPkScript
+    # indexPkScript extracts all standard addresses from the passed public key
+    # script and maps each of them to the associated transaction using the passed
+    # map.
+    def index_pk_script(self, data: WriteIndexData, pk_script: bytes, tx_idx: int):
+        # Nothing to index if the script is non-standard or otherwise doesn't
+        # contain any addresses.
+        try:
+            _, addrs, _ = txscript.extract_pk_script_addrs(pk_script, self.chain_params)
+        except Exception:
+            return
+
+        if len(addrs) == 0:
+            return
+
+        for addr in addrs:
+            try:
+                addr_key = addr_to_key(addr)
+            except Exception:
+                continue
+
+            # TOCONSIDER why
+            # Avoid inserting the transaction more than once.  Since the
+            # transactions are indexed serially any duplicates will be
+            # indexed in a row, so checking the most recent entry for the
+            # address is enough to detect duplicates.
+            indexed_txns = data[addr_key]
+            num_txns = len(indexed_txns)
+            if num_txns > 0 and indexed_txns[-1] == tx_idx:
+                continue
+
+            indexed_txns.append(tx_idx)
+            data[addr_key] = indexed_txns
+
+        return
 
 
+# TODO
+def addr_to_key(addr: btcutil.Address) -> bytes:
+    pass
